e0e91f4e2753d5f9a6be530bf1657242a4d09858
DavidSucias <david.sucias@gft.com>
2021-07-19 12:09:53 +0200

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Sucias Colomer, David on 19/7/21.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
